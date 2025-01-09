45b73397fb8743d94ddf835099b74fdedd3fc873
QWQ916 <Shirok2005@icloud.com>
2025-01-09 20:47:39 +0300

    Add files via upload

---
+++ Classes/InterfaceAccess/Access.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_TP
+{
+    public interface Access
+    {
+        void Log_in(Users user);
+        void LowerAccess(Access access);
+        void ShowQuestion(Users users);
+    }
+}
