ff75fe15792bf2fdad21f2f48401153f736e8c78
QWQ916 <Shirok2005@icloud.com>
2025-01-09 20:50:22 +0300

    Add files via upload

---
+++ Classes/BannedUsers/BannedUsers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_TP
+{
+    public class BannedUsers
+    {
+        private string login; public string Login { get => login; }
+        private string mess; public string Message { get => mess; }
+        public BannedUsers(string login, string mess)
+        {
+            this.login = login; this.mess = mess;
+        }
+        public BannedUsers()
+        {
+            
+        }
+    }
+}
