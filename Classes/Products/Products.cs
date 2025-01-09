2902bf045012fc5364293a61faa6eeae89b06546
QWQ916 <Shirok2005@icloud.com>
2025-01-09 20:48:29 +0300

    Add files via upload

---
+++ Classes/Products/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_TP
+{
+    public class Products
+    {
+        public int id; string name; int count; int price;
+        public Products(int id, string name, int count)
+        {
+            this.id = id; this.name = name; this.count = count;
+        }
+        public Products(string name, int price, int count)
+        {
+            this.name = name; this.count = count; this.price = price;
+        }
+
+        public string Name { get => name; }
+        public int Count { get => count; }
+        public int Price { get => price; }
+    }
+}
