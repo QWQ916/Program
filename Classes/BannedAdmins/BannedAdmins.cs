24962d85a95bc0e776524128e6d58a65474188ec
QWQ916 <Shirok2005@icloud.com>
2025-01-09 20:49:51 +0300

    Add files via upload

---
+++ Classes/BannedAdmins/BannedAdmins.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_TP
+{
+    public class BannedAdmins
+    {
+        private string login; public string Login { get => login; }
+        private string mess; public string Message { get => mess; }
+        public BannedAdmins(string login, string mess)
+        {
+            this.login = login; this.mess = mess;
+        }
+        public BannedAdmins()
+        {
+
+        }
+    }
+}
